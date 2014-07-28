d7ae7ced2adda4ecdcf4e7f6c0c59e172350c6d2
Pooja Saxena <pooja.saxena@cern.ch>
2014-07-28 20:49:16 +0200

    Added the compiled/tested CMSSW_5_3_14 framework

--- setenvscripts.sh
+++ setenvscripts.sh
@@ -3,6 +3,7 @@ USER=$(whoami)
 kinit $USER@CERN.CH
 echo 'INFO: Setting Grid env and proxy'
 source /afs/cern.ch/project/gd/LCG-share/3.2.11-1/etc/profile.d/grid-env.sh
+
 voms-proxy-init -voms cms
 {
     echo 'INFO: Setting agent environment'
@@ -17,6 +18,7 @@ voms-proxy-init -voms cms
     echo 'ERROR: This machine does not have git installed. Not updating WmAgentScripts repository'
 }
 
+echo 'INFO: Setting Global Variables for POOJA'
 git config --global user.name  "Pooja Saxena"
 git config --global user.email pooja.saxena@cern.ch
 git config --global core.editor "emacs -nw"
